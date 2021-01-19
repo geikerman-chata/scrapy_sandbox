1a0bddda855308c0ed518a7e5117ed1fe22f65a6
geikerman-chata <geikerman@chata.ai>
2021-01-19 15:39:56 -0700

    compile.py with multiprocessing added. First text on linux

--- babyscrape/compile.py
+++ babyscrape/compile.py
@@ -2,25 +2,13 @@ from google.cloud import storage
 import json
 from pathlib import Path
 from split_output import split_reviews
-from multiprocessing import Pool
-from multiprocessing import get_context
+from multiprocessing import Pool, get_context
 import argparse
+
 
 class InvalidArgument(Exception):
     pass
 
-client = storage.Client()
-bucket = client.bucket('nlp_resources')
-print("Fetching Bucket List, takes a minute...")
-full_list = list(bucket.list_blobs(prefix='ta-crawler/raw-output-3'))
-print("Number of files in bucket: {}".format(len(full_list)))
-
-
-done_list = {}
-parser = argparse.ArgumentParser()
-parser.add_argument("--workers", "-n", help="Number of workers to split up work")
-parser.add_argument("--half", "-d", help="which half of the full list the workers will work on, 1 or 2")
-args = parser.parse_args()
 
 def blob_2_dict(blob):
     blob_dict = None
@@ -30,6 +18,7 @@ def blob_2_dict(blob):
         if json_data:
             blob_dict = json_data[0]
     return blob_dict
+
 
 def divine_args(args, full_list):
     if args.half:
@@ -56,20 +45,40 @@ def divine_args(args, full_list):
     return task_list, num_workers
 
 
-task_list, num_workers = divine_args(args, full_list)
-bunch_increment = int(round(float(len(task_list))/num_workers))
-task_chunks = [task_list[x:x+bunch_increment] for x in range(0, len(task_list), bunch_increment)]
-
-bucket_name = 'nlp_resources'
-bucket_sub_dir = 'ta-crawler/test/'
-worker_number = 0
-
-
-def split_google_blob(worker_number, task_chunks):
-    for blob in task_chunks[worker_number]:
+def split_google_blob(task_chunk, worker_id, bucket_name, bucket_sub_dir):
+    print('Worker Number: {}'.format(worker_id))
+    for blob in task_chunk:
         blob_dict = blob_2_dict(blob)
         if blob_dict:
             split_reviews(bucket_name, blob_dict, bucket_sub_dir)
+
+
+def main():
+    client = storage.Client()
+    bucket = client.bucket('nlp_resources')
+    print("Fetching Google Bucket list, takes a minute...")
+    full_list = list(bucket.list_blobs(prefix='ta-crawler/raw-output-3'))
+    print("Number of files in bucket: {}".format(len(full_list)))
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--workers", "-n", help="Number of workers to split up work")
+    parser.add_argument("--half", "-d", help="which half of the full list the workers will work on, 1 or 2")
+    args = parser.parse_args()
+
+    task_list, num_workers = divine_args(args, full_list)
+    bunch_increment = int(round(float(len(task_list))/num_workers))
+    task_chunks = [task_list[x:x+bunch_increment] for x in range(0, len(task_list), bunch_increment)]
+
+    bucket_name = 'nlp_resources'
+    bucket_sub_dir = 'ta-crawler/test/'
+
+    with Pool(processes=num_workers) as pool:
+        pool.starmap(split_google_blob,
+                     [(task_chunks[i], i, bucket_name, bucket_sub_dir) for i in range(0, num_workers)])
+
+
+if __name__ =="__main__":
+    main()
 
 
 

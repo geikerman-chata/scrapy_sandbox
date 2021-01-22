eeb0d0af8a4fcb712184ec8fba970864d30abfaa
geikerman-chata <geikerman@chata.ai>
2021-01-22 15:41:25 -0700

    Added arguments to data_rollup.py

--- babyscrape/data_rollup.py
+++ babyscrape/data_rollup.py
@@ -4,17 +4,23 @@ from pathlib import Path
 from split_output import split_reviews
 from multiprocessing import Pool, get_context
 import argparse
-
+from langdetect import detect
 
 class InvalidArgument(Exception):
     pass
-
 
 def blob_2_dict(blob):
     bytes_data = blob.download_as_string()
     if bytes_data:
         json_data = json.loads(bytes_data)
     return json_data
+
+def check_language(review):
+    try:
+        lang = detect(review)
+    except:
+        lang = None
+    return lang
 
 def rollup_chunk(task_chunk, chunk_name, bad_reviews):
     rolling_reviews = {}
@@ -26,13 +32,15 @@ def rollup_chunk(task_chunk, chunk_name, bad_reviews):
             data_key = data_key_list[0]
             if "review" in review_dict[data_key].keys():
                 if 'review_text' in review_dict[data_key]['review']:
-                    if len(review_dict[data_key]['review']['review_text']) < 780 \
-                            and review_dict[data_key]['review']['review_text'][-1] != '…':
-                        rolling_reviews[data_key] = review_dict[data_key]
+                    if len(review_dict[data_key]['review']['review_text']) > 760 \
+                            and review_dict[data_key]['review']['review_text'][-1] == '…':
+                        bad_reviews[data_key] = "truncated"
+                    elif check_language(review_dict[data_key]['review']['review_response']) != 'en':
+                        bad_reviews[data_key] = "response not english"
                     else:
-                        bad_reviews[data_key] = review_dict
+                        rolling_reviews[data_key] = review_dict[data_key]
 
-    upload_json_blob('nlp_resources', rolling_reviews, 'ta-crawler/{}.json'.format(chunk_name))
+    upload_json_blob('nlp_resources', rolling_reviews, chunk_name)
     del rolling_reviews
     return bad_reviews
 
@@ -51,18 +59,46 @@ def upload_json_blob(bucket_name, json_data, destination_blob_name):
 def main():
     client = storage.Client()
     bucket = client.bucket('nlp_resources')
-    print("Fetching Google Bucket list, takes a minute...")
-    full_list = list(bucket.list_blobs(prefix='ta-crawler/response/en'))
-    print("Number of files in bucket: {}".format(len(full_list)))
-    bad_reviews = {}
-    bunch_increment = 250000
-    task_chunks = [full_list[x:x + bunch_increment] for x in range(0, len(full_list), bunch_increment)]
-    del full_list
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--chunk_start", "-s", help="chunk start")
+    parser.add_argument("--chunk_end", "-f", help="chunk finish")
+    parser.add_argument("--chunk_size", "-s", help="worker id")
+    parser.add_argument("--subdir", "-p", help="Subdirectory to roll up")
+    args = parser.parse_args()
 
-    for file_number, chunk in enumerate(task_chunks):
-        chunk_name = 'en_reviews_{}.json'.format(str(file_number+1))
-        bad_reviews = rollup_chunk(chunk, chunk_name, bad_reviews)
-    upload_json_blob('nlp_resources', bad_reviews, 'ta-crawler/{}.json'.format('truncated_en_reviews_list.json'))
+    args.subdir = 'ta-hotel/response/en'
+    args.chunk_size = '250000'
+    args.chunk_start = '0'
+    args.chunk_end = '1'
+
+    if args.subdir:
+        print("Fetching Google Bucket list, takes a minute...")
+        subdir = args.subdir
+        full_list = list(bucket.list_blobs(prefix=args.subdir))
+        if len(full_list) == 0:
+            raise InvalidArgument("given subdirectory is empty")
+        print("Number of files in bucket: {}".format(len(full_list)))
+    if args.chunk_size:
+        bunch_increment = int(args.chunk_size)
+        task_chunks = [full_list[x:x + bunch_increment] for x in range(0, len(full_list), bunch_increment)]
+        if args.chunk_start and args.chunk_end:
+            chunk_range = (int(args.chunk_start), int(args.chunk_end))
+        else:
+            chunk_range = (0, len(task_chunks))
+    else:
+        bunch_increment = 250000
+    bad_reviews = {}
+
+    del full_list
+    task = task_chunks[chunk_range[0]:chunk_range[1]]
+    del task_chunks
+
+    subdir_top = subdir.split('/')[0]
+    for file_number, chunk in enumerate(task):
+        chunk_save_path = '{}/en_reviews_{}.json'.format(subdir_top, str(file_number+1))
+        bad_reviews = rollup_chunk(chunk, chunk_save_path, bad_reviews)
+    if bad_reviews:
+        upload_json_blob('nlp_resources', bad_reviews, '{}/{}.json'.format(subdir_top, 'bad_reviews'))
 
 if __name__ == "__main__":
     main()
